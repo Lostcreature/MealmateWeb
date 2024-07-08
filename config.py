8fe747c411e4cbf5311574fba2d54a27b40851c5
Manish Kumar <manishkumarzira08@gmail.com>
2024-07-08 18:43:23 +0530

    Initial commit

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'wdfghbdfhfhfhfhfhgdhsdhdfhfhfghgjkhgjg'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:admin@mysql_db/mealmate?charset=utf8mb4&collation=utf8mb4_general_ci'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:admin@localhost/mealmate'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
